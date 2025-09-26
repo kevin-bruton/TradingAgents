ecca9b1efc652d3058635095c1fed4c771f018a5
Kevin Bruton <kevin.jose.bs@gmail.com>
2025-09-26 16:59:04 +0200

    feat: add dotenv loading

--- cli/main.py
+++ cli/main.py
@@ -19,6 +19,8 @@ from rich.tree import Tree
 from rich import box
 from rich.align import Align
 from rich.rule import Rule
+from dotenv import load_dotenv
+load_dotenv()
 
 from tradingagents.graph.trading_graph import TradingAgentsGraph
 from tradingagents.default_config import DEFAULT_CONFIG
